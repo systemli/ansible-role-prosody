23aeccb436859ff02d496c04f12b519e6c4e0ab0
shadow <shadow@systemli.org>
2017-09-08 23:45:58 +0200

    [prosody] - moved to molecule v2 - linted as much code as possible - lint disabled due to false positives in ./molecule dir

--- tests/test_default.py
+++
@@ -1,9 +0,0 @@
-from testinfra.utils.ansible_runner import AnsibleRunner
-
-testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
-
-
-def test_prosody_running_and_enabled(Service):
-    prosody = Service("prosody")
-    assert prosody.is_running
-    assert prosody.is_enabled
